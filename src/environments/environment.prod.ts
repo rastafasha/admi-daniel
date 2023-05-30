e48ae4ea4f568e9222fff8919bf1b0439c93000c
Malcolm Cordova <mercadocreativo@gmail.com>
2023-05-30 15:25:42 -0400

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  //local
+  // apiUrl: "http://127.0.0.1:8000/api",
+  // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
+  //remoto
+  apiUrl: "https://ursigalletti.net/backend-api-daniel/public/api",
+  apiUrlMedia: "https://ursigalletti.net/backend-api-daniel/storage/app/",
+  clientSandboxId: 'AXlazeNsZ0CmjfJIronSzcqzw4hLHkcoVEM5fO5BY7AbD-_GhKoKezRcavq6-T4kQuRqaTXFB_VXmheG',
+  clientId: ''
+
+};
